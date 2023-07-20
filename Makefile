835ed234898972cd58777cf4d042e2a749e7bd6b
Hir4 <hirayama.rafael517@gmail.com>
2023-07-20 11:40:21 -0300

    chore: add tests to db connection

--- Makefile
+++ Makefile
@@ -2,3 +2,8 @@ MAKEFLAGS += --always-make
 
 run_app:
 	docker compose up
+
+test_db:
+	docker compose up -d
+	pytest -v -m "db"
+	docker compose down
---
+++ tests/test_write_db.py
@@ -0,0 +1,18 @@
+import pytest
+
+@pytest.mark.db
+def test_write_db(db_conn):
+  cur = db_conn.cursor()
+  test_write_query = """
+    CREATE TABLE test_write(
+       test VARCHAR PRIMARY KEY
+    );
+    SELECT EXISTS (
+      SELECT FROM information_schema.tables 
+      WHERE table_name = 'test_write'
+    );
+  """
+  cur.execute(test_write_query)
+  result = cur.fetchone()
+  cur.close()
+  assert result[0] == True
