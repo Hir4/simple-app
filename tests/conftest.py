6177d200a2f2100c563cab7b4dfa88656e2f7c61
Rafael H Hirayama <80469863+Hir4@users.noreply.github.com>
2023-07-20 11:45:40 -0300

    chore: add tests to db connection (#8)
    * chore: add tests to db connection
    
    * style: black lint

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+import psycopg2
+
+
+@pytest.fixture
+def db_conn():
+    conn = psycopg2.connect(
+        database="db_simple_app",
+        host="localhost",
+        user="user_fael",
+        password="test123",
+        port="5432",
+    )
+
+    yield conn
+
+    conn.close()
