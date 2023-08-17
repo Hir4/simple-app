53bd1c69afd0a5acc4467bf8818d3fd2b1712522
Rafael H Hirayama <80469863+Hir4@users.noreply.github.com>
2023-08-17 17:21:12 -0300

    refact: tests with fastpi client (#16)
    * refact: tests with fastpi client
    
    * remove: script to run tests

--- tests/test_get_accounts.py
+++ tests/test_get_accounts.py
@@ -1,20 +1,29 @@
 import pytest
+from fastapi.testclient import TestClient
 
-# DEFAULT
-url = "http://localhost:8080"
+from app.main import app
+
+client = TestClient(app)
 
 
 @pytest.mark.app
 @pytest.mark.account
-def test_get_account_by_name(httpx_request):
-    response = httpx_request.get(f"{url}/get_accounts_by_name/Fael")
+def test_get_account_by_name(mocker):
+    mocker.patch(
+        "app.db_functions.get_account_by_name",
+        return_value=["1", "Fael", "teste123", "2023-08-02 00:00:00"],
+    )
+    account_name = "Fael"
+    response = client.get(f"/get_account_by_name/{account_name}")
     assert response.status_code == 200
-    assert "Fael" in response.json()["detail"]["message"]
+    assert account_name in response.json()["detail"]["message"]
 
 
 @pytest.mark.app
 @pytest.mark.account
-def test_get_inexisting_accounts_by_name(httpx_request):
-    response = httpx_request.get(f"{url}/get_accounts_by_name/rodrigo_goes056061189156")
+def test_get_inexisting_accounts_by_name(mocker):
+    mocker.patch("app.db_functions.get_account_by_name", return_value=None)
+    account_name = "some_name"
+    response = client.get(f"/get_account_by_name/{account_name}")
     assert response.status_code == 404
-    assert response.json()["detail"]["message"] == "Account not found"
+    assert "Account not found" == response.json()["detail"]["message"]
--- tests/test_save_weather_data.py
+++ tests/test_save_weather_data.py
@@ -1,12 +1,16 @@
 import pytest
+from fastapi.testclient import TestClient
 
+from app.main import app
+
+client = TestClient(app)
 # DEFAULT
-url = "http://localhost:8080"
 header = {"Content-Type": "application/json"}
 # CONTENT
-content_send_weather = {
-    "latitude": -23.5475,
-    "longitude": -46.6361,
+state_coordinates = {"SP": {"lat": -23.5475, "long": -46.6361}}
+content_weather_api = {
+    "latitude": state_coordinates["SP"]["lat"],
+    "longitude": state_coordinates["SP"]["long"],
     "start_date": "2023-07-14",
     "end_date": "2023-08-03",
 }
@@ -14,9 +18,27 @@ content_send_weather = {
 
 @pytest.mark.app
 @pytest.mark.weather
-def test_save_weather_data(httpx_request):
-    response = httpx_request.post(
-        f"{url}/historical_weather/", headers=header, json=content_send_weather
+def test_save_weather_data(mocker):
+    mocker.patch(
+        "app.db_functions.insert_weather_table",
+        return_value={
+            "id": "213231321",
+            "latitude": state_coordinates["SP"]["lat"],
+            "longitude": state_coordinates["SP"]["long"],
+            "start_date": "2023-07-14",
+            "end_date": "2023-08-03",
+            "inserted_at": "2023-08-10",
+        },
+    )
+    response = client.post(
+        "/historical_weather/", headers=header, json=content_weather_api
     )
     assert response.status_code == 200
-    assert response.json()["detail"]["message"] == "Weather saved successfully"
+    assert (
+        response.json()["detail"]["message"]["latitude"]
+        == state_coordinates["SP"]["lat"]
+    )
+    assert (
+        response.json()["detail"]["message"]["longitude"]
+        == state_coordinates["SP"]["long"]
+    )
